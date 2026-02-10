53a072e1ecfdedccfae073c34cc8b04320ddf6f6
Zach <zacharyeverett.00@gmail.com>
2026-02-11 01:36:24 +1100

    recording of replays working

--- recording.py
+++ recording.py
@@ -1,12 +1,64 @@
 from pyfastscreencap import pyfastscreencap as screencap
 from time import sleep
+from utils import find_location_on_screen
+import math
+import pyautogui
 
-recorder = screencap.Recorder("video.mp4", 0, 60, 100, True)
+FRAME_RATE = 60
 
-recorder.start_recording()
+locations = {
+    "speed_button": (2460, 1340),
+    "pause_button": (2325, 1340)
+}
 
-sleep(3)
+def toggle_pause():
+    pyautogui.click(*locations["pause_button"])
 
-frame_count = recorder.stop_recording()
+def speed_up_game():
+    pyautogui.click(*locations["speed_button"])
 
-print(f"Recorded {frame_count} frames")
+# returns replay length in seconds
+def record_replay(filepath, speed_factor=1):
+    if not speed_factor in (1, 2, 4):
+        raise ValueError("Speed factor must be 1 of: 1, 2, 4")
+    
+    while not find_location_on_screen("resources/pause_button.png", 0.99):
+        sleep(0.1)
+    
+    #initially pause while doing setup
+    toggle_pause()
+
+    #zoom out
+    pyautogui.moveTo(1000, 1000)
+    for _ in range(10):
+        pyautogui.scroll(-500)
+
+    #unpause to begin recording
+    toggle_pause()
+
+    #handling game speed
+    speed_button_clicks = round(math.log2(speed_factor))
+    for _ in range(speed_button_clicks):
+        speed_up_game()
+
+    #recording
+    recorder = screencap.Recorder(filepath, 0, FRAME_RATE, 100, True)
+
+    recorder.start_recording()
+    
+    #check if play again button is on screen to end replay
+    while not find_location_on_screen("resources/play_again.png", 0.99):
+        sleep(1)
+
+    frame_count = recorder.stop_recording()
+
+    replay_time = frame_count / FRAME_RATE * speed_factor
+
+    print(f"Recorded {frame_count} frames")
+    print(f"Recorded {replay_time} second replay")
+
+    return 
+
+if __name__ == "__main__":
+    pyautogui.PAUSE = 0.5
+    record_replay("replay.mp4", 2)
