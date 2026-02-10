9e3fcbe0a5768c97220fd779bfaee4a587b57e11
Zach <zacharyeverett.00@gmail.com>
2026-02-10 23:33:04 +1100

    cleaned up main a bit

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import pyautogui
 from utils import init_clash_window
 from time import sleep
 
+#set a 0.5 second delay after every pyautogui call to allow ui updates and prevent misclicks
+pyautogui.PAUSE = 0.5
+
 init_clash_window()
 
+locations = {
+    "cwl_menu": (80, 975),
+    "close_popup": (2256, 40)
+}
 #open cwl menu
-pyautogui.click(80, 975)
-sleep(1)
+pyautogui.click(*locations["cwl_menu"])
 
 #click red x to close popup if cwl is over and summary is up
-pyautogui.click(2256, 40)
-sleep(1)
+pyautogui.click(*locations["close_popup"])
